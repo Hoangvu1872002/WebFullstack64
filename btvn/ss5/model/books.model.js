82fd4e8e105ae5fd10520bede2d9b3e6a58e58f1
Hoangvu1872002 <hoangvu1872k2@gmail.com>
2023-02-28 17:41:25 +0700

    update

---
+++ btvn/ss5/model/books.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose') 
+const booksSchema = mongoose.Schema;
+
+const bookSchema = new booksSchema ({
+    
+    title: String,
+    author: String,
+    publication_date: String,
+    pages: Number,
+    genres: [String, String, String],
+    publisher:{
+        name: String,
+        location: String
+    }
+})
+
+module.exports = mongoose.model('book', bookSchema);
+
---
+++ btvn/ss5/routes/books.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const booksRouter = express.Router();
+const booksModel = require('../model/books.model');
+
+booksRouter.post('/', (req, res) => {
+    const book = new booksModel();
+    book.title = req.body.title;
+    book.author = req.body.author;
+    book.publication_date = req.body.publication_date;
+    book.pages = req.body.pages;
+    book.genres[0] = req.body.genres[0];
+    book.genres[1] = req.body.genres[1];
+    book.genres[2] = req.body.genres[2];
+
+    book.publisher.name = req.body.publisher.name;
+    book.publisher.location = req.body.publisher.location;
+
+    // book.save((err, book) => {
+    //     if (err) {
+    //         res.send("error");
+    //     } else {
+    //         console.log('win', book);
+    //         res.json(book);
+    //     }
+    // })
+
+    booksModel.insertMany(book, (err) => {
+        if (err) {
+            res.send("error")
+        } else {
+            res.send(book);
+        }
+    })
+})
+
+booksRouter.get("/", (req, res) => {
+    booksModel.find({ title: req.query.title }).exec((err, book) => {
+        if (err) {
+            res.send("error");
+        } else {
+            console.log("success");
+            res.json(book)
+        }
+    })
+})
+
+booksRouter.put("/:id", (req, res) => {
+    booksModel.findOneAndUpdate({
+        _id: req.params.id
+    }, {
+        $set: {
+            title: req.body.title,
+            author: req.body.author,
+            publication_date: req.body.publication_date,
+            pages: req.body.pages,
+
+            genres: [req.body.genres[0], req.body.genres[1], req.body.genres[2]],
+            publisher:{
+                name: req.body.publisher.name,
+                location: req.body.publisher.location,
+            }
+        }
+    }, {
+        upsert: true
+
+    },
+        (err, book) => {
+            if (err) {
+                res.send("error")
+            } else {
+                res.send(book)
+            }
+        }
+
+    )
+})
+
+
+booksRouter.delete("/:id", (req, res) => {
+    booksModel.findByIdAndDelete({
+        _id: req.params.id
+    }, {
+
+    }, (err) => {
+        if (err) {
+            res.send("error")
+        } else {
+            res.status(200).send("thanh cong!")
+        }
+    }
+    )
+})
+
+module.exports = booksRouter;
